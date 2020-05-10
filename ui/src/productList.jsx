56303030a0eb0cfd2404956651b16d2d204e826c
Romil <romilshah6462@gmail.com>
2020-05-09 17:22:30 -0700

    Error solved for 0 products

--- ui/src/productList.jsx
+++ ui/src/productList.jsx
@@ -58,7 +58,11 @@ export default class ProductList extends React.Component {
       body: JSON.stringify({ query }),
     });
     const result = await response.json();
-    this.setState({ count: result.data.productCounts[0].count });
+    if (result.data.productCounts.length > 0) {
+      this.setState({ count: result.data.productCounts[0].count });
+    } else {
+      this.setState({ count: 0 });
+    }
   }
 
   async createProduct(product) {
